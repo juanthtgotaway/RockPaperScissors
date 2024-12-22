fca848b597d7a30d6f335b7a27061cb264b53289
Juan Acevedo <jmamartinez18@gmail.com>
2024-12-22 11:04:02 -0700

    got second option generated for  randomName

--- WhoWillPay/bill.py
+++ WhoWillPay/bill.py
@@ -3,3 +3,7 @@ import random
 friends = ["Juan", "Paola", "Jason", "Erick", "Stephanie", "David", "Christa"]
 
 print(random.choice(friends))
+
+# Other options: 
+randomName = random.randint(0,7)
+print(friends[randomName])
