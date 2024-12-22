5df472221b6eb673b207fcd6aa6ed87f6d05fc31
Juan Acevedo <jmamartinez18@gmail.com>
2024-12-22 11:16:38 -0700

    final touches

--- indexError/test.py
+++ indexError/test.py
@@ -1,2 +1,8 @@
-dirty_dozen = ["Strawberries", "Spinach", "Kale", "Nectarines", "Apples", "Grapes", "Peaches", "Cherries", "Pears",]
+# dirty_dozen = ["Strawberries", "Spinach", "Kale", "Nectarines", "Apples", "Grapes", "Peaches", "Cherries", "Pears",]
 
+fruits = ["Strawberries", "Nectarines", "Apples", "Grapes", "Peaches", "Cherries", "Pears"]
+vegetables = ["Spinach", "Kale", "Tomatoes", "Celery", "Potatoes"]
+
+dirtyDozen = [fruits, vegetables]
+
+print(dirtyDozen[0][3])
