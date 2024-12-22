80368d7664946ad82f6520bd3231597c7cc0841b
Juan Acevedo <jmamartinez18@gmail.com>
2024-12-22 07:48:06 -0700

    added folders and base file for upcoming topics

---
+++ RandomModule/test.py
@@ -0,0 +1,10 @@
+import random
+
+# randomInt=random.randint(1,10)
+# print(randomInt)
+
+randomNumber = random.random()
+print(randomNumber)
+
+randomFloat = random.uniform(1,10)
+print(randomFloat)
