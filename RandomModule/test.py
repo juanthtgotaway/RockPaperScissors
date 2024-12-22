8baadbd9f48c722e0b5479bc5b4fb9152dd826d6
Juan Acevedo <jmamartinez18@gmail.com>
2024-12-22 08:16:19 -0700

    created rock paper scissors

--- RandomModule/test.py
+++ RandomModule/test.py
@@ -3,8 +3,30 @@ import random
 # randomInt=random.randint(1,10)
 # print(randomInt)
 
-randomNumber = random.random()
-print(randomNumber)
+# randomNumber = random.random()
+# print(randomNumber)
 
-randomFloat = random.uniform(1,10)
-print(randomFloat)
+# randomFloat = random.uniform(1,10)
+# print(randomFloat)
+
+# MyCode:
+input('How lucky are you feeling? Pick heads or tails by responding "heads" or "tails\n').lower()
+if input == "heads":
+    compChoice = random.randint(1,2)
+    if compChoice == 1:
+        print('You\'re fate chose tails')
+    else:
+        print('You\'re fate chose heads')
+else:
+    compChoice = random.randint(1,2)
+    if compChoice == 2:
+        print('You\'re fate chose tails')
+    else:
+        print('You\'re fate chose heads')
+
+# course answer
+# compChoice = random.randint(0, 1)
+# if compChoice == 0: 
+#     print("Heads")
+# else:
+#     print("Tails")
